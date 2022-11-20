9578650265ea4551ff2351cb3de70ce7e85f0edc
sophiecra <77420992+sophiecra@users.noreply.github.com>
2022-11-20 17:26:45 -0500

    Added feed title and border

---
+++ app/feed/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+const Feed = () => (
+    <div className={styles.main}>
+        <div className={styles.title}>Feed</div>
+        <div className={styles.border}></div>
+    </div>
+)
+
+export default Feed;
