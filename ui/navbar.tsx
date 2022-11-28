424b09a61cefc5ee652e93d914774a7134a0ab16
Dalton Craven <daltoncraven@proton.me>
2022-11-27 22:39:29 -0500

    feat: create menu component
    #28

---
+++ ui/navbar.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+export type ItemType = {
+  label: string;
+  url: string;
+};
+
+const NavbarItem = ({ item }: { item: ItemType }) => (
+  <li>
+    <Link className="normal-case font-bold" href={item.url}>
+      {item.label}
+    </Link>
+  </li>
+);
+
+const navbarItems = [
+  {
+    label: "Feed",
+    url: "/",
+  },
+  {
+    label: "Profile",
+    url: "/profile",
+  },
+];
+
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="navbar-start">
+      <h1>
+        <Link
+          className="normal-case text-xl btn btn-ghost rounded-none text-primary"
+          href="/"
+        >
+          DKMS
+        </Link>
+      </h1>
+    </div>
+    <div className="navbar-end">
+      <ul className="menu menu-horizontal">
+        {navbarItems.map((item) => (
+          <NavbarItem item={item} key={item.url} />
+        ))}
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Navbar;
