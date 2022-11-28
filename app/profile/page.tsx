d8e734efb5ff207c0e40d731af2dbad569a1f901
sophiecra <77420992+sophiecra@users.noreply.github.com>
2022-11-28 18:57:55 -0500

    Merged in feed_page and made changes for tailwind

---
+++ app/profile/page.tsx
@@ -0,0 +1,12 @@
+import FeedItem from "../feed/FeedItem";
+import "../globals.css";
+
+const Profile = () => {
+    return <div>
+        <h1>Profile</h1>
+        <div className="divider-vertical"></div>
+        <FeedItem />
+    </div>
+}
+
+export default Profile;
