4483ddcca54db59efdd3787c42dac64fb3149808
sophiecra <77420992+sophiecra@users.noreply.github.com>
2022-11-30 16:12:59 -0500

    feat: added props for profile feed items

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,8 +4,8 @@ import "../globals.css";
 const Profile = () => (
   <div>
     <h1 className="normal-case font-bold">Profile</h1>
-    <div className="divider-vertical" />
-    <FeedItem />
+    <div className="divider" />
+    <FeedItem username=" " feedContent=" " />
   </div>
 );
 
