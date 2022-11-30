3adb445e75e66387c2ee616e1a14a46631a85063
Dalton Craven <daltoncraven@proton.me>
2022-11-30 13:49:09 -0500

    feat: add additional info from Spotify to session data
    #19

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,5 +1,7 @@
 import Footer from "../components/ui/Footer";
 
+import UserInfo from "./UserInfo";
+
 import type { User } from "next-auth";
 
 const Home = ({ user }: { user: User }) => (
@@ -9,8 +11,13 @@ const Home = ({ user }: { user: User }) => (
         Welcome to <a href="https://nextjs.org">Next.js 13</a>, {user.name}!
       </h1>
 
-      <p>Here is some information about yourself (according to Spotify):</p>
+      <p>
+        Here is some <em>server-side</em> information about yourself (according
+        to Spotify):
+      </p>
       <pre>{JSON.stringify(user, null, 2)}</pre>
+      <hr />
+      <UserInfo />
     </main>
 
     <hr />
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { unstable_getServerSession } from "next-auth/next";
 
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+
 import Home from "./Home";
 
 const Page = async () => {
-  const session = await unstable_getServerSession();
+  const session = await unstable_getServerSession(authOptions);
 
-  return <Home user={session!.user!} />;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return <Home user={session!.user} />;
 };
 
 export default Page;
