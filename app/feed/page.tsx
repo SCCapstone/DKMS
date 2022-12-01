ca5f8147c8f845dfbd7b1e7926316947407fa3de
sophiecra <77420992+sophiecra@users.noreply.github.com>
2022-11-30 20:04:24 -0500

    feat: added calls to database from feed

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+"use client";
+
+import { onSnapshot, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+
+import db from "../firebase";
 
 import FeedItem from "./FeedItem";
 
-const Feed = () => (
-  <div>
-    <h1 className="normal-case font-bold">Feed</h1>
-    <div className="divider" />
-    <FeedItem username=" " feedContent=" " />
-  </div>
-);
+import type { DocumentData } from "firebase/firestore";
+
+const Feed = () => {
+  const [usernames, setUsernames] = useState<DocumentData[]>([]);
+
+  console.log(usernames);
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "feed_content"), (snapshot) =>
+        setUsernames(snapshot.docs.map((doc) => doc.data()))
+      ),
+    []
+  );
+
+  return (
+    <div>
+      <h1 className="normal-case font-bold">Feed</h1>
+      <div className="divider" />
+      <ul>
+        {usernames.map((username) => (
+          // TODO FIX THIS SOPHIE
+          <FeedItem
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            username={username.username}
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            feedContent={username.content}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Feed;
