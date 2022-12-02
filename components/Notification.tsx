e3bbc1ea24a69ae6f0f07c6b52407ea6135ee922
masondjoseph <mdjoseph@email.sc.edu>
2022-12-02 16:08:38 -0500

    #22 more eslint revisions

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,11 +1,15 @@
 import "../app/globals.css";
 
 const Notification = () => (
-    <div>
+  <div>
     <div className="h-fit">
       <div className="flex flex-row justify-between items-center">
-        <div><h1 className="normal-case font-bold">Title</h1></div>
-        <div><h1 className="normal-case font-bold">HH:MM PM</h1></div>
+        <div>
+          <h1 className="normal-case font-bold">Title</h1>
+        </div>
+        <div>
+          <h1 className="normal-case font-bold">HH:MM PM</h1>
+        </div>
       </div>
       <div>Body</div>
     </div>
