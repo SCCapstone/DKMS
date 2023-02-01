4b31d3682bcc00d7a8cf17b7f666f943ee0289a0
Dalton Craven <daltoncraven@proton.me>
2023-02-01 13:41:02 -0500

    fix: temporarily remove localStorage for sidebar

--- components/sidebar/SidebarProvider.tsx
+++ components/sidebar/SidebarProvider.tsx
@@ -1,34 +1,14 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
-
-import { getLocalStorage, setLocalStorage } from "../../lib/localStorage";
+import { useState } from "react";
 
 import SidebarContext from "./SidebarContext";
-import { isSidebarOption } from "./types";
 
 import type { SidebarOptions } from "./types";
 
 const SidebarProvider = ({ children }: { children: React.ReactNode }) => {
-  const [currentSidebar, setCurrentSidebar] = useState<SidebarOptions>("none");
+  const sidebarContextValue = useState<SidebarOptions>("notifications");
 
-  useEffect(() => {
-    const storedSidebar = getLocalStorage<string>("sidebar");
-
-    if (isSidebarOption(storedSidebar)) {
-      setCurrentSidebar(storedSidebar);
-    }
-  }, [setCurrentSidebar]);
-
-  const handleSidebarChange = (option: SidebarOptions) => {
-    setCurrentSidebar(option);
-    setLocalStorage("sidebar", option);
-  };
-
-  const sidebarContextValue = useMemo(
-    () => [currentSidebar, handleSidebarChange] as const,
-    [currentSidebar]
-  );
   return (
     <SidebarContext.Provider value={sidebarContextValue}>
       {children}
