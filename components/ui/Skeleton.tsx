70802ca545e7419cb69e443a2f17f7defa8853d3
Dalton Craven <daltoncraven@proton.me>
2023-02-06 21:21:58 -0500

    fix: update docs for Skeleton wrapper

--- components/ui/Skeleton.tsx
+++ components/ui/Skeleton.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-// We have to do this because the react-loading-skeleton package
-// doesn't have a client directive.
-// https://beta.nextjs.org/docs/rendering/server-and-client-components#third-party-packages
+/**
+ * We have to do this because the react-loading-skeleton package
+ * doesn't have a client directive.
+ *
+ * @see https://beta.nextjs.org/docs/rendering/server-and-client-components#third-party-packages
+ * @see https://github.com/dvtng/react-loading-skeleton/pull/162
+ */
 import Skeleton from "react-loading-skeleton";
 
 export default Skeleton;
