8a109483f70770cd45bb6ffa8e94a49bcacdd271
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-02-08 17:58:42 -0500

    feat: added text box to feed page

--- components/feed/FeedPage.tsx
+++ components/feed/FeedPage.tsx
@@ -13,6 +13,13 @@ const FeedPage = ({
   showLinks?: boolean;
 }) => (
   <div>
+    <div>
+      <textarea
+        id="message"
+        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Share your musical thoughts..."
+      />
+    </div>
     <div className="divider" />
     <ul>
       {data.map((feedItem) => (
