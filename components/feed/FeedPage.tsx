d333d00afc451f286b35bd92f25d540000b2d298
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-02-09 09:19:54 -0500

    feat: added text area and post button

--- components/feed/FeedPage.tsx
+++ components/feed/FeedPage.tsx
@@ -13,13 +13,26 @@ const FeedPage = ({
   showLinks?: boolean;
 }) => (
   <div>
-    <div>
-      <textarea
-        id="message"
-        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Share your musical thoughts..."
-      />
-    </div>
+    <form>
+      <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+          <textarea
+            id="comment"
+            className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+            placeholder="Write your musical thoughts..."
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+          <button
+            type="submit"
+            className="inline-flex items-center py-2.5 px-4 text-xs font-medium bg-primary text-center text-white rounded-lg focus:ring-4 "
+          >
+            Post
+          </button>
+        </div>
+      </div>
+    </form>
     <div className="divider" />
     <ul>
       {data.map((feedItem) => (
