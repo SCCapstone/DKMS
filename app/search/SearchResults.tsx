65a021080b7a61f0442e7c2f65a0ed872df861d6
Dalton Craven <daltoncraven@proton.me>
2023-03-05 00:03:58 -0500

    feat: show DKMS users in search if any match

--- app/search/SearchResults.tsx
+++ app/search/SearchResults.tsx
@@ -2,6 +2,9 @@ import Album from "components/music/Album";
 import Artist from "components/music/Artist";
 import Playlist from "components/music/Playlist";
 import Track from "components/music/Track";
+import UsernameLink from "components/ui/UsernameLink";
+
+import type { FirestoreUser } from "lib/firestore/types";
 
 const LOADING_ITEMS = {
   albums: {
@@ -16,14 +19,16 @@ const LOADING_ITEMS = {
   playlists: {
     items: Array<undefined>(4).fill(undefined),
   },
+  users: undefined,
 } as const;
 
-const SearchResults = ({
-  results,
-}: {
-  results: SpotifyApi.SearchResponse | undefined;
-}) => {
-  const { albums, tracks, artists, playlists } = results ?? LOADING_ITEMS;
+type SearchResultsProps = {
+  results: (SpotifyApi.SearchResponse & { users: FirestoreUser[] }) | undefined;
+};
+
+const SearchResults = ({ results }: SearchResultsProps) => {
+  const { albums, tracks, artists, playlists, users } =
+    results ?? LOADING_ITEMS;
 
   return (
     <div>
@@ -55,6 +60,18 @@ const SearchResults = ({
           <Playlist key={playlist?.id ?? index} playlist={playlist} />
         ))}
       </div>
+      {users && (
+        <>
+          <h2 className="font-black">DKMS Users</h2>
+          <ul>
+            {users.map((user) => (
+              <li>
+                <UsernameLink username={user.name} />
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,33 @@
+import { getDocs, query, where } from "firebase/firestore";
+
+import { usersCol } from "lib/firestore/firebase";
 import getSpotifyData from "lib/getSpotifyData";
 
 import SearchResults from "./SearchResults";
+
+const searchFirebase = async (searchQuery: string) => {
+  const q = query(
+    usersCol,
+    where("name", ">=", searchQuery),
+    where("name", "<=", `${searchQuery}\uf8ff`)
+  );
+
+  const usersSnapshot = await getDocs(q);
+  return usersSnapshot.docs.map((doc) => doc.data());
+};
+const searchSpotify = async (searchQuery: string) =>
+  getSpotifyData<SpotifyApi.SearchResponse>(
+    `https://api.spotify.com/v1/search?q=${searchQuery}&type=album,track,artist,playlist&limit=4`
+  );
 
 const getData = async (searchQuery: string | undefined) => {
   if (typeof searchQuery !== "string") {
     return undefined;
   }
+  const spotifyResults = await searchSpotify(searchQuery);
+  const firebaseResults = await searchFirebase(searchQuery);
 
-  return getSpotifyData<SpotifyApi.SearchResponse>(
-    `https://api.spotify.com/v1/search?q=${searchQuery}&type=album,track,artist,playlist&limit=4`
-  );
+  return { ...spotifyResults, users: firebaseResults };
 };
 
 const Page = async ({
@@ -18,6 +36,7 @@ const Page = async ({
   searchParams: { q: undefined | string };
 }) => {
   const results = await getData(searchParams.q);
+
   return searchParams.q ? <SearchResults results={results} /> : <div />;
 };
 
