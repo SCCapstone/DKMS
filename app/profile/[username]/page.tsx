714a9085d1bcb7fda71e9d38888b5ee205e8e9ef
Dalton Craven <daltoncraven@proton.me>
2023-03-06 23:36:01 -0500

    feat: get profile data from firestore

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,9 +1,20 @@
+import { doc, getDoc } from "firebase/firestore";
 import { notFound } from "next/navigation";
 
 import FollowButton from "@/components/profile/FollowButton";
 import ProfileHead from "@/components/profile/ProfileHead";
+import { profilesCol } from "@/lib/firestore";
 import isUserFollowing from "@/lib/followers/isUserFollowing";
 import { getCurrentUser, getUserByUsername } from "@/lib/getUser";
+
+const getData = async (id: string) => {
+  const profileDoc = await getDoc(doc(profilesCol, id));
+  if (!profileDoc.exists()) {
+    notFound();
+  }
+
+  return profileDoc.data();
+};
 
 const Profile = async ({ params }: { params: { username: string } }) => {
   const currentUser = await getCurrentUser();
@@ -18,6 +29,8 @@ const Profile = async ({ params }: { params: { username: string } }) => {
   const isFollowed = await isUserFollowing(profile.id);
   const showFollowButton = username !== currentUsername;
 
+  const data = await getData(profile.id);
+
   return (
     <>
       <ProfileHead user={profile} />
@@ -25,6 +38,7 @@ const Profile = async ({ params }: { params: { username: string } }) => {
         <FollowButton isFollowing={isFollowed} username={username} />
       )}
       <div className="divider" />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 };
