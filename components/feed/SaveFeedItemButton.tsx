814654d9c820bcd4c76dda168bc32aaa8200bf16
claycrews2002 <claycrews2002@gmail.com>
2023-03-19 00:54:42 -0400

    feat: add save item button functionality

---
+++ components/feed/SaveFeedItemButton.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+
+import { saveFeedItem, unsaveFeedItem } from "@/lib/savedFeedItems";
+
+const SaveFeedItemButton = ({
+  userId,
+  postId,
+  savedItemIds,
+}: {
+  userId: string;
+  postId: string;
+  savedItemIds: string[];
+}) => {
+  const itemId = savedItemIds.find((item) => item.trim() === postId);
+  const saved = postId === itemId?.trim();
+
+  const [localSavedState, setLocalSavedState] = useState(saved);
+
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+
+  // Create inline loading UI
+  const isMutating = isFetching || isPending;
+
+  const handleClick = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsFetching(true);
+    if (localSavedState) {
+      setLocalSavedState(!localSavedState);
+      await unsaveFeedItem(userId, postId);
+    } else {
+      setLocalSavedState(!localSavedState);
+      await saveFeedItem(userId, postId);
+    }
+    setIsFetching(false);
+    startTransition(() => {
+      // Refresh the current route and fetch new data from the server without
+      // losing client-side browser or React state.
+      router.refresh();
+    });
+  };
+
+  if (!localSavedState) {
+    return (
+      <div>
+        <svg
+          onClick={(e) => void handleClick(e)}
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="black"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-disabled={isMutating}
+        >
+          <path
+            d="M19 21L12 16L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z"
+            stroke="white"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <svg
+        onClick={(e) => void handleClick(e)}
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="white"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-disabled={isMutating}
+      >
+        <path
+          d="M19 21L12 16L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default SaveFeedItemButton;
