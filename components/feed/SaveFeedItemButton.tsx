79abf62054c04ecc08e00320e653cbaa6b2c87a9
claycrews2002 <claycrews2002@gmail.com>
2023-03-20 00:11:33 -0400

    fix: clean up save button

--- components/feed/SaveFeedItemButton.tsx
+++ components/feed/SaveFeedItemButton.tsx
@@ -43,37 +43,13 @@ const SaveFeedItemButton = ({
     });
   };
 
-  if (!localSavedState) {
-    return (
-      <div>
-        <svg
-          onClick={(e) => void handleClick(e)}
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="black"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-disabled={isMutating}
-        >
-          <path
-            d="M19 21L12 16L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </div>
-    );
-  }
   return (
-    <div>
+    <button type="button" onClick={(e) => void handleClick(e)}>
       <svg
-        onClick={(e) => void handleClick(e)}
         width="24"
         height="24"
         viewBox="0 0 24 24"
-        fill="white"
+        fill={localSavedState ? "white" : "black"}
         xmlns="http://www.w3.org/2000/svg"
         aria-disabled={isMutating}
       >
@@ -85,7 +61,7 @@ const SaveFeedItemButton = ({
           strokeLinejoin="round"
         />
       </svg>
-    </div>
+    </button>
   );
 };
 
