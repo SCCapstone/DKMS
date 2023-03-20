b5f155acd9611420283f10311c66ebc892dc6897
claycrews2002 <claycrews2002@gmail.com>
2023-03-19 23:51:28 -0400

    fix: return like and unlike item

--- lib/likeButton/unlikeItem.ts
+++ lib/likeButton/unlikeItem.ts
@@ -2,10 +2,9 @@ import { arrayRemove, doc, updateDoc } from "firebase/firestore";
 
 import { feedCol } from "@/lib//firestore";
 
-const unlikeItem = async (userId: string, postId: string) => {
-  await updateDoc(doc(feedCol, postId), {
+const unlikeItem = async (userId: string, postId: string) =>
+  updateDoc(doc(feedCol, postId), {
     likedIds: arrayRemove(userId),
   });
-};
 
 export default unlikeItem;
