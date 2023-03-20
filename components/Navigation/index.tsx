d2c0fb64418f8e163b222ab495812ab4317b4ecc
claycrews2002 <claycrews2002@gmail.com>
2023-03-20 15:31:40 -0400

    feat: add recommendations to navigation

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -17,6 +17,10 @@ const NAVBAR_ITEMS: MenuItems = [
     url: "/search",
   },
   {
+    label: "Recommended",
+    url: "/recommendations",
+  },
+  {
     label: "Settings",
     url: "/settings",
   },
