bc2125f922af78e68e7f33b2725239d5fb88a555
Kevin Nguyen <kn11@email.sc.edu>
2023-03-22 12:21:49 -0400

    Add Playback buttons to feed

--- app/(feed)/page.tsx
+++ app/(feed)/page.tsx
@@ -4,6 +4,9 @@ import getUser from "utils/getUser";
 import FeedPage from "../../components/feed/FeedPage";
 import PageTitle from "../../components/ui/PageTitle";
 
+import SpotifyWebApi from 'spotify-web-api-js';
+import PlaybackBar from "/home/kn11/Documents/fix490/DKMS/app/playbackbar/bar";
+
 import type { FeedItemContent } from "../../components/feed/FeedPage";
 
 const Feed = async () => {
@@ -12,10 +15,11 @@ const Feed = async () => {
 
   return (
     <div>
+      <PlaybackBar />
       <PageTitle title="Feed" />
       <FeedPage data={data} user={user} showLinks />
     </div>
   );
 };
-
+const spotifyApi = new SpotifyWebApi();
 export default Feed;
