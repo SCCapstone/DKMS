91437bb23a286f67133ebf7f16bb16c8a0620dcb
claycrews2002 <claycrews2002@gmail.com>
2023-03-23 16:21:09 -0400

    fix: style save button

--- components/feed/SaveFeedItemButton.tsx
+++ components/feed/SaveFeedItemButton.tsx
@@ -44,14 +44,17 @@ const SaveFeedItemButton = ({
   };
 
   return (
-    <button type="button" onClick={(e) => void handleClick(e)}>
+    <button
+      className="btn btn-ghost"
+      type="button"
+      onClick={(e) => void handleClick(e)}
+    >
       <svg
         width="24"
         height="24"
         viewBox="0 0 24 24"
         className={localSavedState ? "fill-white" : "fill-base-100"}
         xmlns="http://www.w3.org/2000/svg"
-        aria-disabled={isMutating}
       >
         <path
           d="M19 21L12 16L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z"
