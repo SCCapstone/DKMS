5c492a1e1d1680c7e0ce59581d3693f46c65c0cd
claycrews2002 <claycrews2002@gmail.com>
2023-03-23 16:48:08 -0400

    fix: like button loading and style

--- components/feed/LikeButton.tsx
+++ components/feed/LikeButton.tsx
@@ -22,8 +22,8 @@ const LikeButton = ({
   // Created this "local" state to fix how long it would take between
   //  clicking the like button and like button being updated
   // Then still updates to current with database
-  const [localLikesNum, setLocalLikesNum] = useState(likes);
-  const [localLikedState, setLocalLikedState] = useState(liked);
+  // const [localLikesNum, setLocalLikesNum] = useState(likes);
+  // const [localLikedState, setLocalLikedState] = useState(liked);
 
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
@@ -35,13 +35,13 @@ const LikeButton = ({
   const handleClick = async (e: React.MouseEvent) => {
     e.preventDefault();
     setIsFetching(true);
-    if (localLikedState) {
-      setLocalLikesNum(localLikesNum - 1);
-      setLocalLikedState(!localLikedState);
+    if (liked) {
+      // setLocalLikesNum(localLikesNum - 1);
+      // setLocalLikedState(!localLikedState);
       await unlikeItem(userId, postId);
     } else {
-      setLocalLikesNum(localLikesNum + 1);
-      setLocalLikedState(!localLikedState);
+      // setLocalLikesNum(localLikesNum + 1);
+      // setLocalLikedState(!localLikedState);
       await likeItem(userId, postId);
     }
     setIsFetching(false);
@@ -53,10 +53,14 @@ const LikeButton = ({
   };
 
   return (
-    <div className="flex flex-row">
-      <button type="button" onClick={(e) => void handleClick(e)}>
+    <div className="flex flex-row items-center">
+      <button
+        className={`btn btn-ghost ${isMutating ? "loading" : ""}`}
+        type="button"
+        onClick={(e) => void handleClick(e)}
+      >
         <svg
-          fill={localLikedState ? "white" : "black"}
+          className={liked ? "fill-white" : "fill-base-100"}
           width="24"
           height="24"
           viewBox="0 0 24 24"
@@ -65,14 +69,15 @@ const LikeButton = ({
         >
           <path
             d="M5.57256 9.09995L9.2306 1C9.95823 1 10.6561 1.28446 11.1706 1.79081C11.6851 2.29715 11.9741 2.9839 11.9741 3.69998V7.29996H17.1503C17.4154 7.297 17.678 7.35081 17.9199 7.45765C18.1618 7.5645 18.3772 7.72182 18.5512 7.91872C18.7251 8.11562 18.8535 8.34739 18.9275 8.59798C19.0014 8.84856 19.0191 9.11197 18.9793 9.36995L17.7173 17.4699C17.6511 17.8991 17.4296 18.2903 17.0935 18.5715C16.7574 18.8527 16.3293 19.0048 15.8882 18.9999H5.57256M5.57256 9.09995V18.9999M5.57256 9.09995H2.82902C2.34394 9.09995 1.87872 9.28959 1.53571 9.62715C1.1927 9.96472 1 10.4226 1 10.8999V17.1999C1 17.6773 1.1927 18.1351 1.53571 18.4727C1.87872 18.8102 2.34394 18.9999 2.82902 18.9999H5.57256"
-            stroke={localLikedState ? "black" : "white"}
+            stroke={liked ? "black" : "white"}
+            className={liked ? "stroke-base-100" : "stroke-white"}
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
           />
         </svg>
       </button>
-      <p>{formatNumber(localLikesNum)}</p>
+      <p>{formatNumber(likes)}</p>
     </div>
   );
 };
