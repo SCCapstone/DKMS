16c542b344be01567be2cd2cfc0fa040a07b2687
Dalton Craven <daltoncraven@proton.me>
2023-03-23 10:23:07 -0400

    feat: switch to next-themes for theme manager

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { useEffect } from "react";
-import { themeChange } from "theme-change";
+import { ThemeProvider } from "next-themes";
 
 import SidebarProvider from "@/components/Sidebar/SidebarProvider";
+import { THEME_VALUES } from "@/lib/theme";
 
-const Providers = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    themeChange(false);
-  }, []);
-
-  return (
-    <SessionProvider>
-      <SidebarProvider>{children}</SidebarProvider>
-    </SessionProvider>
-  );
-};
+const Providers = ({ children }: { children: React.ReactNode }) => (
+  <SessionProvider>
+    <SidebarProvider>
+      <ThemeProvider themes={[...THEME_VALUES]}>{children}</ThemeProvider>
+    </SidebarProvider>
+  </SessionProvider>
+);
 
 export default Providers;
