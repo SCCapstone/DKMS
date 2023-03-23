98d5b2875e16d6511f986af5b5227118942f41f7
claycrews2002 <claycrews2002@gmail.com>
2023-03-23 16:10:17 -0400

    fix: use className to style

--- components/feed/SaveFeedItemButton.tsx
+++ components/feed/SaveFeedItemButton.tsx
@@ -49,7 +49,7 @@ const SaveFeedItemButton = ({
         width="24"
         height="24"
         viewBox="0 0 24 24"
-        fill={localSavedState ? "white" : "black"}
+        className={localSavedState ? "fill-white" : "fill-base-100"}
         xmlns="http://www.w3.org/2000/svg"
         aria-disabled={isMutating}
       >
