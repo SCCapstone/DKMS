4a335411dd90c095a10fb37eb6d5dbf518bb4ff2
Dalton Craven <daltoncraven@proton.me>
2023-03-23 16:01:37 -0400

    feat: add duration formatter

--- lib/formatters.ts
+++ lib/formatters.ts
@@ -13,4 +13,18 @@ export const formatNumber = (count: number | undefined) => {
   return `${(count / 1000000000).toFixed(1)}b`;
 };
 
-export default {};
+/**
+ * Takes a duration in milliseconds and returns it in minutes:seconds
+ * or hours:minutes:seconds if the duration is over an hour.
+ * @param ms The duration in milliseconds
+ * @returns The duration in hours:minutes:seconds
+ */
+export const formatDuration = (ms: number) => {
+  const date = new Date(ms);
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const seconds = date.getUTCSeconds().toString().padStart(2, "0");
+  return hours > 0
+    ? `${hours}:${minutes.toString().padStart(2, "0")}:${seconds}`
+    : `${minutes}:${seconds}`;
+};
