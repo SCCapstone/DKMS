e1b2c3d86092c62327e3d6186f7db38396781352
Kevin Nguyen <kn11@email.sc.edu>
2023-03-24 19:55:27 -0400

    fixed index and update playback

--- components/Sidebar/panels/index.tsx
+++ components/Sidebar/panels/index.tsx
@@ -2,13 +2,19 @@ import Friends from "./Friends";
 import Notifications from "./Notifications";
 import Playback from "./Playback/Playback";
 
-const SidebarPanels = ({ isDesktop }: { isDesktop?: boolean }) => (
-  <div className={isDesktop ? "hidden md:block" : "md:hidden"}>
-    <Friends />
-    {/* @ts-expect-error Next 13 handles async components */}
-    <Notifications />
-    <Playback />
-  </div>
-);
+const SidebarPanels = ({ isDesktop }: { isDesktop?: boolean }) => {
+  // Assume you have the following values for isTrackPlaying and uri
+  const isTrackPlaying = true;
+  const uri = "spotify:track:6y0igZArWVi6Iz0rj35c1Y";
+
+  return (
+    <div className={isDesktop ? "hidden md:block" : "md:hidden"}>
+      <Friends />
+      {/* @ts-expect-error Next 13 handles async components */}
+      <Notifications />
+      <Playback isTrackPlaying={isTrackPlaying} uri={uri} />
+    </div>
+  );
+};
 
 export default SidebarPanels;
