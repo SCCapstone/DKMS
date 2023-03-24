cb6ce9c71cf0a9a017e1639fa7419121c2f8edb7
Dalton Craven <daltoncraven@proton.me>
2023-03-24 14:18:24 -0400

    feat: only join unique artists

--- lib/joinArtists.ts
+++ lib/joinArtists.ts
@@ -2,10 +2,11 @@ const joinArtists = (
   artists: Pick<SpotifyApi.ArtistObjectSimplified, "name">[] | undefined
 ) => {
   if (!artists) return "";
-  if (artists.length === 0) return "";
-  if (artists.length === 1) return artists[0].name;
-  if (artists.length === 2) return `${artists[0].name} & ${artists[1].name}`;
-  return `${artists[0].name} & ${artists.length - 1} more`;
+  const uniqueNames = Array.from(new Set(artists.map((a) => a.name)));
+  if (uniqueNames.length === 0) return "";
+  if (uniqueNames.length === 1) return uniqueNames[0];
+  if (uniqueNames.length === 2) return `${uniqueNames[0]} & ${uniqueNames[1]}`;
+  return `${uniqueNames[0]} & ${uniqueNames.length - 1} more`;
 };
 
 export default joinArtists;
