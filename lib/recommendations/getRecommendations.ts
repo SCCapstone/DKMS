47fe68dcd65552bd72adc51fbc96fd5ea20c0d9c
claycrews2002 <claycrews2002@gmail.com>
2023-03-24 17:31:01 -0400

    fix: undefined profile data case

--- lib/recommendations/getRecommendations.ts
+++ lib/recommendations/getRecommendations.ts
@@ -7,11 +7,12 @@ import { profilesCol } from "../firestore";
 const getRecommendations = async (userId: string) => {
   const profile = await getDoc(doc(profilesCol, userId));
 
-  const artist1 = profile.data()?.topArtists[0].id ?? "4NHQUGzhtTLFvgF5SZesLK";
-  const artist2 = profile.data()?.topArtists[1].id ?? "4NHQUGzhtTLFvgF5SZesLK";
-  const track1 = profile.data()?.topTracks[0].id ?? "0c6xIDDpzE81m2q797ordA";
-  const track2 = profile.data()?.topTracks[1].id ?? "0c6xIDDpzE81m2q797ordA";
-  const track3 = profile.data()?.topTracks[2].id ?? "0c6xIDDpzE81m2q797ordA";
+  const data = profile.data();
+  const artist1 = data?.topArtists[0].id ?? "4NHQUGzhtTLFvgF5SZesLK";
+  const artist2 = data?.topArtists[1].id ?? "4NHQUGzhtTLFvgF5SZesLK";
+  const track1 = data?.topTracks[0].id ?? "0c6xIDDpzE81m2q797ordA";
+  const track2 = data?.topTracks[1].id ?? "0c6xIDDpzE81m2q797ordA";
+  const track3 = data?.topTracks[2].id ?? "0c6xIDDpzE81m2q797ordA";
 
   return getSpotifyData<SpotifyApi.RecommendationsFromSeedsResponse>(
     `https://api.spotify.com/v1/recommendations?seed_artists=${artist1},${artist2}&
