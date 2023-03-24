a83594a069249f89443754be73a133cbb6d2997d
claycrews2002 <claycrews2002@gmail.com>
2023-03-24 17:51:48 -0400

    fix: error when topArtists/topTracks are undefined

--- lib/recommendations/getRecommendations.ts
+++ lib/recommendations/getRecommendations.ts
@@ -8,11 +8,13 @@ const getRecommendations = async (userId: string) => {
   const profile = await getDoc(doc(profilesCol, userId));
 
   const data = profile.data();
-  const artist1 = data?.topArtists[0].id ?? "4NHQUGzhtTLFvgF5SZesLK";
-  const artist2 = data?.topArtists[1].id ?? "4NHQUGzhtTLFvgF5SZesLK";
-  const track1 = data?.topTracks[0].id ?? "0c6xIDDpzE81m2q797ordA";
-  const track2 = data?.topTracks[1].id ?? "0c6xIDDpzE81m2q797ordA";
-  const track3 = data?.topTracks[2].id ?? "0c6xIDDpzE81m2q797ordA";
+  const topArtists = data?.topArtists ?? [];
+  const topTracks = data?.topTracks ?? [];
+  const artist1 = topArtists[0]?.id ?? "4NHQUGzhtTLFvgF5SZesLK";
+  const artist2 = topArtists[1]?.id ?? "4NHQUGzhtTLFvgF5SZesLK";
+  const track1 = topTracks[0]?.id ?? "0c6xIDDpzE81m2q797ordA";
+  const track2 = topTracks[1]?.id ?? "0c6xIDDpzE81m2q797ordA";
+  const track3 = topTracks[2]?.id ?? "0c6xIDDpzE81m2q797ordA";
 
   return getSpotifyData<SpotifyApi.RecommendationsFromSeedsResponse>(
     `https://api.spotify.com/v1/recommendations?seed_artists=${artist1},${artist2}&
