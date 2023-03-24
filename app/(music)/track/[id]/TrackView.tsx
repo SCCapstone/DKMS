1b03666c2132f51257fe503f645b8a5782b4e098
Dalton Craven <daltoncraven@proton.me>
2023-03-24 13:38:03 -0400

    feat: create Track page

---
+++ app/(music)/track/[id]/TrackView.tsx
@@ -0,0 +1,59 @@
+import { TrackList } from "@/components/music/lists";
+import MusicHeader from "@/components/music/MusicHeader";
+import TrackFeatures from "@/components/music/TrackFeatures";
+import { capitalize, formatDuration } from "@/lib/formatters";
+
+const TrackView = ({
+  track,
+  artist,
+  audioFeatures,
+  recommendations,
+}: {
+  track: SpotifyApi.TrackObjectFull;
+  artist: SpotifyApi.ArtistObjectFull;
+  audioFeatures: SpotifyApi.AudioFeaturesResponse;
+  recommendations: SpotifyApi.RecommendationsFromSeedsResponse;
+}) => (
+  <>
+    <MusicHeader
+      primary={{
+        imageUrl: track.album.images[0].url,
+        defaultImage: "/images/defaults/album.png",
+        uri: track.uri,
+        title: track.name,
+        subtitle: `Released ${new Date(
+          track.album.release_date
+        ).toLocaleDateString()} | ${capitalize(
+          formatDuration(track.duration_ms)
+        )}`,
+        content: track.album.name,
+      }}
+      secondary={{
+        imageUrl: artist.images[0].url,
+        defaultImage: "/images/defaults/artist.png",
+        uri: artist.uri,
+        path: `/artist/${artist.id}`,
+        title: artist.name,
+        isCircle: true,
+      }}
+    />
+    <div className="divider" />
+    <h4 className="font-black uppercase pb-2">Track Statistics</h4>
+    <TrackFeatures audioFeatures={audioFeatures} />
+    <div className="divider" />
+    <h4 className="font-black uppercase">Similar Tracks</h4>
+    <TrackList tracks={recommendations.tracks} showArtist />
+
+    <details>
+      <pre>{JSON.stringify(audioFeatures, null, 2)}</pre>
+    </details>
+    <details>
+      <pre>{JSON.stringify(recommendations, null, 2)}</pre>
+    </details>
+    <details>
+      <pre>{JSON.stringify(track, null, 2)}</pre>
+    </details>
+  </>
+);
+
+export default TrackView;
--- components/music/lists/TrackList.tsx
+++ components/music/lists/TrackList.tsx
@@ -2,30 +2,42 @@ import { formatDuration } from "@/lib/formatters";
 
 const TrackList = ({
   tracks,
+  showNumber,
+  showArtist,
 }: {
   tracks: SpotifyApi.TrackObjectSimplified[];
+  showNumber?: boolean;
+  showArtist?: boolean;
 }) => (
   <div className="overflow-x-auto">
     <table className="table table-compact w-full">
       <thead>
         <tr>
-          <th>#</th>
+          {showNumber && <th>#</th>}
           <th>Title</th>
+          {showArtist && <th>Artist</th>}
           <th className="text-right">Duration</th>
         </tr>
       </thead>
       <tbody>
         {tracks.map((track, index) => (
           <tr className="hover" key={track.id}>
-            <td>{index + 1}</td>
-            <td className="max-w-prose">
-              <a
-                className="font-bold text-primary text-ellipsis "
-                href={track.uri}
-              >
+            {showNumber && <td>{index + 1}</td>}
+            <td>
+              <a className="font-bold text-primary" href={`/track/${track.id}`}>
                 {track.name}
               </a>
             </td>
+            {showArtist && (
+              <td>
+                <a
+                  className="font-bold text-secondary"
+                  href={`/artist/${track.artists[0].id}`}
+                >
+                  {track.artists[0].name}
+                </a>
+              </td>
+            )}
             <td className="text-right">{formatDuration(track.duration_ms)}</td>
           </tr>
         ))}
