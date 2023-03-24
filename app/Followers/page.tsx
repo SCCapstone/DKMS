308da2fdc8ebcdef53ef0dd9ac58df6e2025bec0
Kevin Nguyen <kn11@email.sc.edu>
2023-03-24 13:27:49 -0400

    added page for Followed

---
+++ app/Followers/page.tsx
@@ -0,0 +1,27 @@
+import FeedPage from "@/components/feed";
+import FilterSavedFeedButton from "@/components/feed/FilterSavedFeedButton";
+import PageTitle from "@/components/ui/PageTitle";
+import { getFeedItems } from "@/lib/feed";
+import { getCurrentUser } from "@/lib/getUser";
+import { getSavedFeedItemsList } from "@/lib/savedFeedItems";
+
+const Feed = async () => {
+  const data = await getFeedItems();
+  const currentUser = await getCurrentUser();
+  const savedItemIds = await getSavedFeedItemsList(currentUser.id);
+
+  return (
+    <div>
+      <PageTitle title="Feed" />
+      <FilterSavedFeedButton />
+      <FeedPage
+        data={data}
+        currentUser={currentUser}
+        savedItemIds={savedItemIds}
+        showLinks
+      />
+    </div>
+  );
+};
+
+export default Feed;
