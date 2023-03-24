25663dca3e1011c0046e790d9f338c6593a7341f
Kevin Nguyen <kn11@email.sc.edu>
2023-03-24 13:52:43 -0400

    updated index for playback

--- components/Sidebar/panels/index.tsx
+++ components/Sidebar/panels/index.tsx
@@ -1,11 +1,13 @@
 import Friends from "./Friends";
 import Notifications from "./Notifications";
+import Playback from "./Playback/Playback";
 
 const SidebarPanels = ({ isDesktop }: { isDesktop?: boolean }) => (
   <div className={isDesktop ? "hidden md:block" : "md:hidden"}>
     <Friends />
     {/* @ts-expect-error Next 13 handles async components */}
     <Notifications />
+    <Playback />
   </div>
 );
 
