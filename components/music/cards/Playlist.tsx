a71c642ba54fd919031d033a775216d9e9f76274
claycrews2002 <claycrews2002@gmail.com>
2023-03-25 18:12:44 -0400

    feat: style compact playlist for feed

--- components/music/cards/Playlist.tsx
+++ components/music/cards/Playlist.tsx
@@ -2,16 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 import { getPlaiceholder } from "plaiceholder";
 
+import ShareIcon from "@/components/ui/shareIcon";
 import Skeleton from "@/components/ui/Skeleton";
 
+import type { User } from "next-auth";
+
 const Playlist = async ({
+  user,
   playlist,
+  isCompact,
 }: {
+  user: User;
   playlist: SpotifyApi.PlaylistObjectSimplified | undefined;
+  isCompact?: boolean;
 }) => {
   if (!playlist) {
     return (
-      <div className="card card-compact bg-base-300 hover:bg-base-100 transition shadow-xl overflow-clip">
+      <div
+        className={`card ${
+          isCompact ? "card-side" : "card-compact"
+        } bg-base-300 hover:bg-base-100 transition shadow-xl overflow-clip`}
+      >
         <figure className="relative aspect-square" />
         <div className="card-body">
           <h2 className="text-lg font-semibold truncate">
@@ -41,7 +52,9 @@ const Playlist = async ({
   return (
     <Link
       href={`/playlist/${playlist.id}`}
-      className="card card-compact bg-base-300 hover:bg-base-100 transition shadow-xl overflow-clip"
+      className={`card ${
+        isCompact ? "card-side" : "card-compact"
+      } bg-base-300 hover:bg-base-100 transition shadow-xl overflow-clip`}
     >
       <figure className="relative aspect-square">
         <Image
@@ -52,9 +65,22 @@ const Playlist = async ({
           blurDataURL={base64}
         />
       </figure>
-      <div className="card-body">
-        <h2 className="text-lg font-semibold truncate">{playlist.name}</h2>
-        <p>
+      <div className="card-body relative">
+        <div
+          className={`flex flex-row absolute 
+            ${isCompact ? "bottom-0" : "top-0"}
+          right-0 p-2`}
+        >
+          <ShareIcon user={user} sharedItem={playlist} />
+        </div>
+        <h2
+          className={`text-lg truncate font-semibold  ${
+            isCompact ? "" : "mt-10"
+          }`}
+        >
+          {playlist.name}
+        </h2>
+        <p className={isCompact ? "text-sm truncate" : ""}>
           {playlist.tracks.total}{" "}
           {playlist.tracks.total === 1 ? "track" : "tracks"}
         </p>
