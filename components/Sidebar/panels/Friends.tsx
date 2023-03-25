48243fc9c9fc19205ec35065e37e0e86029b1a38
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-03-25 12:23:13 -0400

    feat: made artists and songs links

--- components/Sidebar/panels/Friends.tsx
+++ components/Sidebar/panels/Friends.tsx
@@ -1,11 +1,12 @@
 import { doc, getDoc } from "firebase/firestore";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import "server-only";
 
-import Track from "@/components/music/Track";
 import UsernameLink from "@/components/ui/UsernameLink";
 import { profilesCol } from "@/lib/firestore";
 import getUsersFollowing from "@/lib/followers/getUsersFollowing";
+import isUserFollowing from "@/lib/followers/isUserFollowing";
 
 import BasePanel from "./BasePanel";
 
@@ -36,14 +37,31 @@ const Friend = async ({
               <UsernameLink username={username}>{username}</UsernameLink>
             ) : (
               username
-            )}
+            )}{" "}
+            is listening to
           </h4>
         </div>
         <div>
-          <ul className="flex-row">
+          <ul className="flex-column">
             {tracks.map((topTrack) => (
-              // @ts-expect-error Next 13 Server Comonent
-              <Track key={topTrack.id} track={topTrack} />
+              <div className="flex-row" key={topTrack.id}>
+                <Link
+                  className="link link-hover link-secondary break-all"
+                  href={topTrack.external_urls.spotify}
+                >
+                  {topTrack.name}
+                </Link>
+                <h5> by </h5>
+                {topTrack.artists.map((artist) => (
+                  <Link
+                    key={artist.id}
+                    href={artist.external_urls.spotify}
+                    className="link link-hover link-neutral"
+                  >
+                    {artist.name}&nbsp;
+                  </Link>
+                ))}
+              </div>
             ))}
           </ul>
         </div>
@@ -59,7 +77,7 @@ const Friends = async () => {
     <BasePanel title="Friends" sidebarId="friends">
       <ul>
         {users.map((user) => (
-          // @ts-expect-error Next 13 Server Comonent
+          // @ts-expect-error Next 13 Server Component
           <Friend key={user.id} username={user.username} userId={user.id} />
         ))}
       </ul>
