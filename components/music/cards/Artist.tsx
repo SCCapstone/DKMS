64ccc099a4d83331d4655c87830149d7e46121e3
claycrews2002 <claycrews2002@gmail.com>
2023-03-26 12:04:05 -0400

    feat: add compact style for artist

--- components/music/cards/Artist.tsx
+++ components/music/cards/Artist.tsx
@@ -2,16 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 import { getPlaiceholder } from "plaiceholder";
 
+import ShareIcon from "@/components/ui/shareIcon";
 import Skeleton from "@/components/ui/Skeleton";
 
+import type { User } from "next-auth";
+
 const Artist = async ({
+  user,
   artist,
+  isCompact,
 }: {
+  user: User;
   artist: SpotifyApi.ArtistObjectFull | undefined;
+  isCompact?: boolean;
 }) => {
   if (!artist) {
     return (
-      <div className="card card-compact bg-base-300 hover:bg-base-100 transition shadow-xl">
+      <div
+        className={`card ${
+          isCompact ? "card-side" : "card-compact"
+        } bg-base-300 hover:bg-base-100 transition shadow-xl`}
+      >
         <div className="pt-8 px-8">
           <figure className="rounded-full overflow-clip relative aspect-square shadow-2xl" />
         </div>
@@ -35,11 +46,18 @@ const Artist = async ({
   return (
     <Link
       href={`/artist/${artist.id}`}
-      className="card card-compact bg-base-300 hover:bg-base-100 transition shadow-xl"
+      className={`card ${
+        isCompact ? "card-side" : "card-compact"
+      } bg-base-300 hover:bg-base-100 transition shadow-xl`}
     >
       <div className="pt-8 px-8">
-        <figure className="rounded-full overflow-clip relative aspect-square shadow-2xl">
+        <figure
+          className={`${
+            isCompact ? "w-32 h-32" : ""
+          } rounded-full overflow-clip relative aspect-square shadow-2xl`}
+        >
           <Image
+            className={isCompact ? "w-full h-full" : ""}
             src={img}
             alt={artist.name}
             fill
@@ -48,7 +66,16 @@ const Artist = async ({
           />
         </figure>
       </div>
-      <div className="card-body items-center text-center">
+      <div
+        className={`card-body ${isCompact ? "" : "items-center text-center"}`}
+      >
+        <div
+          className={`flex flex-row absolute 
+            ${isCompact ? "bottom-0" : "top-0"}
+          right-0 p-2`}
+        >
+          <ShareIcon user={user} sharedItem={artist} />
+        </div>
         <h2 className="card-title">{artist.name}</h2>
       </div>
     </Link>
