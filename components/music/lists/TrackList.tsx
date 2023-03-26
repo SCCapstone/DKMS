be3fb69ea597a95f0152992fae02b99a71e6c698
claycrews2002 <claycrews2002@gmail.com>
2023-03-26 15:22:07 -0400

    feat: add extra data to track

--- components/music/lists/TrackList.tsx
+++ components/music/lists/TrackList.tsx
@@ -1,12 +1,17 @@
 import ArtistLinks from "@/components/ui/ArtistLinks";
 import { formatDuration } from "@/lib/formatters";
 
+import type { extraDataTrack } from "@/app/(music)/playlist/[id]/page";
+
 const TrackList = ({
   tracks,
   showNumber,
   showAlbum,
 }: {
-  tracks: SpotifyApi.TrackObjectSimplified[] | SpotifyApi.TrackObjectFull[];
+  tracks:
+    | SpotifyApi.TrackObjectSimplified[]
+    | SpotifyApi.TrackObjectFull[]
+    | extraDataTrack[];
   showNumber?: boolean;
   showAlbum?: boolean;
 }) => (
@@ -18,6 +23,8 @@ const TrackList = ({
           <th>Title</th>
           <th>Artist</th>
           {showAlbum && "album" in tracks[0] && <th>Album</th>}
+          {"added_at" in tracks[0] && <th>Date added</th>}
+          {"added_by" in tracks[0] && <th>Added by</th>}
           <th className="text-right">Duration</th>
         </tr>
       </thead>
@@ -40,6 +47,8 @@ const TrackList = ({
                 </a>
               </td>
             )}
+            {"added_at" in track && <td>{track.added_at}</td>}
+            {"added_by" in track && <td>{track.added_by.display_name}</td>}
             <td className="text-right">{formatDuration(track.duration_ms)}</td>
           </tr>
         ))}
