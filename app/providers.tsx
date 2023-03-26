de6210824925063ddaf3dcbf17c259701c17d8ab
Dalton Craven <daltoncraven@proton.me>
2023-03-26 17:06:41 -0400

    chore: install react-hot-toast

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
 
 import SidebarProvider from "@/components/Sidebar/SidebarProvider";
 import { THEME_VALUES } from "@/lib/theme";
@@ -10,6 +11,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => (
   <SessionProvider>
     <SidebarProvider>
       <ThemeProvider disableTransitionOnChange themes={[...THEME_VALUES]}>
+        <Toaster />
         {children}
       </ThemeProvider>
     </SidebarProvider>
