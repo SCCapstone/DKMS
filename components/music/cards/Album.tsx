7310aa9559952fb30505e44094e02ac90c9842cf
Dalton Craven <daltoncraven@proton.me>
2023-03-27 00:27:56 -0400

    fix: prevent hydration errors for ArtistLinks

--- components/music/cards/Album.tsx
+++ components/music/cards/Album.tsx
@@ -13,7 +13,18 @@ const Album = async ({
   if (!album) {
     return (
       <div className="card card-compact bg-base-300 hover:bg-base-100 transition shadow-xl overflow-clip">
-        <figure className="relative aspect-square" />
+        <figure className="relative aspect-square">
+          <Image
+            src="/images/defaults/album.png"
+            alt="Loading..."
+            fill
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAp0lEQVR4nF2PIQ6GMAyFq5A7zcwE54LMcgdwJDgSBJIEsRtgJrkAAoct3SNkhH8/dX1fX9tH+K8QAjOLSGwpBSGEzzSlGMC+73Vdz/MMQERuHFet65rneZZlRFQUBQBmvvF5ngDGcdRaT9PknKuqKuo/d9u2Xdcdx2GMKcvycTMzgKZpiGgYBu+9Uqrv+8cdrcuyWGu3bXtvfT9P470JHywizBzVNP0FPFwFIjVg8fAAAAAASUVORK5CYII="
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              25vw"
+          />
+        </figure>
         <div className="card-body">
           <h2 className="text-lg font-semibold truncate">
             <Skeleton enableAnimation />
@@ -35,27 +46,31 @@ const Album = async ({
   });
 
   return (
-    <Link
-      href={`/album/${album.id}`}
-      className="card card-compact bg-base-300 hover:bg-base-100 transition shadow-xl overflow-clip"
-    >
-      <figure className="relative aspect-square">
-        <Image
-          src={img}
-          alt={album.name}
-          fill
-          placeholder="blur"
-          blurDataURL={base64}
-        />
-      </figure>
+    <div className="card card-compact bg-base-300 hover:bg-base-100 transition shadow-xl overflow-clip">
+      <Link href={`/album/${album.id}`}>
+        <figure className="relative aspect-square">
+          <Image
+            src={img}
+            alt={album.name}
+            fill
+            placeholder="blur"
+            blurDataURL={base64}
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              25vw"
+          />
+        </figure>
+      </Link>
       <div className="card-body">
-        <h2 className="text-lg font-semibold truncate">{album.name}</h2>
-        <p>
-          {new Date(album.release_date).getFullYear()} |{" "}
+        <Link href={`/album/${album.id}`}>
+          <h2 className="text-lg font-semibold truncate">{album.name}</h2>
+          <p>{new Date(album.release_date).getFullYear()}</p>
+        </Link>
+        <p className="pb-0">
           <ArtistLinks artists={album.artists} />
         </p>
       </div>
-    </Link>
+    </div>
   );
 };
 
