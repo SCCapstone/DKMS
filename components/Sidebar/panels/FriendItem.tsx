e417b7613f276a612922f085b90a1d18460c031e
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-03-27 18:33:18 -0400

    fix: fix truncating of artists and track

--- components/Sidebar/panels/FriendItem.tsx
+++ components/Sidebar/panels/FriendItem.tsx
@@ -1,19 +1,20 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import ArtistLinks from "@/components/ui/ArtistLinks";
 
 const FriendItem = ({ track }: { track: SpotifyApi.TrackObjectFull }) => (
   <li>
-    <div className="flex flex-row justify-start max-w-[224px]">
-      <h5 className="text-xs truncate">
+    <div className="flex flex-row justify-start content-center max-w-[224px]">
+      <h5 className="text-xs truncate max-w-[112px]">
         <Link
-          className="link link-hover link-secondary max-w-[100px]"
+          className="link link-hover link-secondary truncate max-w-[100px]"
           href={track.external_urls.spotify}
         >
           {track.name}
         </Link>
-        &nbsp;•&nbsp;
+      </h5>
+      <h5>&nbsp;•&nbsp;</h5>
+      <h5 className="text-xs truncate max-w-[112px]">
         <ArtistLinks artists={track.artists} />
       </h5>
     </div>
