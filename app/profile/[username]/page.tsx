cb0934aa64d0f99242d507c45ec4bba5d7ec0e45
Dalton Craven <daltoncraven@proton.me>
2023-03-26 21:41:41 -0400

    feat: add audio features to profile page

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -2,10 +2,12 @@ import { doc, getDoc } from "firebase/firestore";
 import { notFound } from "next/navigation";
 
 import FollowButton from "@/components/FollowButton";
+import AudioFeatures from "@/components/music/AudioFeatures";
 import ProfileHead from "@/components/profile/ProfileHead";
 import TopItems from "@/components/profile/TopItems";
 import { profilesCol } from "@/lib/firestore";
 import isUserFollowing from "@/lib/followers/isUserFollowing";
+import getAverageAudioFeatures from "@/lib/getAverageAudioFeatures";
 import getSpotifyData from "@/lib/getSpotifyData";
 import { getCurrentUser, getIdFromUsername } from "@/lib/getUser";
 
@@ -21,7 +23,18 @@ const getTopItems = async (id: string) => {
     notFound();
   }
 
-  return profileDoc.data();
+  const data = profileDoc.data();
+
+  const audioFeatures =
+    await getSpotifyData<SpotifyApi.MultipleAudioFeaturesResponse>(
+      `https://api.spotify.com/v1/audio-features?ids=${data.topTracks
+        .map((track) => track.id)
+        .join(",")}`
+    );
+
+  const averageAudioFeatures = getAverageAudioFeatures(audioFeatures);
+
+  return { data, averageAudioFeatures };
 };
 
 const Profile = async ({ params }: { params: { username: string } }) => {
@@ -39,7 +52,7 @@ const Profile = async ({ params }: { params: { username: string } }) => {
 
   const showFollowButton = username !== currentUsername;
 
-  const data = await getTopItems(profileId);
+  const { data, averageAudioFeatures } = await getTopItems(profileId);
 
   return (
     <>
@@ -51,6 +64,9 @@ const Profile = async ({ params }: { params: { username: string } }) => {
           followType="user"
         />
       )}
+      <div className="divider" />
+      <h4 className="font-black uppercase pb-2">Top Songs Statistics</h4>
+      <AudioFeatures audioFeatures={averageAudioFeatures} />
       <div className="divider" />
       <TopItems artists={data.topArtists} tracks={data.topTracks} />
     </>
