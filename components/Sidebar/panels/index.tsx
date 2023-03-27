7d65693410de3f8fd9c21a290aadbd3882e617c6
Kevin Nguyen <kn11@email.sc.edu>
2023-03-26 22:57:03 -0400

    update

--- components/Sidebar/panels/index.tsx
+++ components/Sidebar/panels/index.tsx
@@ -1,11 +1,11 @@
 import Friends from "./Friends";
 import Notifications from "./Notifications";
-import Playback from "./Playback/Playback";
+import Playback from "./Playback";
 
 const SidebarPanels = ({ isDesktop }: { isDesktop?: boolean }) => {
   // Assume you have the following values for isTrackPlaying and uri
   const isTrackPlaying = false;
-  const uri = "spotify:track:6y0igZArWVi6Iz0rj35c1Y";
+  const uri = "spotify:track:";
   return (
     <div className={isDesktop ? "hidden md:block" : "md:hidden"}>
       <Friends />
