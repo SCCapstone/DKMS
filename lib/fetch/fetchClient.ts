0ccc07c763af03d1059c2e236454e261dc1fea85
Dalton Craven <daltoncraven@proton.me>
2023-03-28 23:23:00 -0400

    docs: add comments to fetch utils

--- lib/fetch/fetchClient.ts
+++ lib/fetch/fetchClient.ts
@@ -8,6 +8,17 @@ type ClientRequestInit = RequestInit & {
   method: "PUT" | "PATCH" | "DELETE";
 };
 
+/**
+ * Fetches data and automatically includes a Spotify
+ * access token credential. For client use only.
+ * Fetching on the client can only use the
+ * methods PUT, PATCH, and DELETE and must
+ * explicitly declare one
+ *
+ * @param input URL to fetch
+ * @param init fetch params
+ * @returns Promise of the response body T
+ */
 const fetchClient = async <T>(
   input: RequestInfo | URL,
   init: ClientRequestInit
--- lib/fetch/fetchServer.ts
+++ lib/fetch/fetchServer.ts
@@ -4,6 +4,14 @@ import getServerAccessToken from "@/lib/getServerAccessToken";
 
 import baseFetch from "./baseFetch";
 
+/**
+ * Fetches data and automatically includes a Spotify
+ * access token credential. For server use only.
+ *
+ * @param input URL to fetch
+ * @param init optional fetch params
+ * @returns Promise of the response body T
+ */
 const fetchServer = async <T>(
   input: RequestInfo | URL,
   init?: RequestInit | undefined
