3a37b8b4f6cb91c33498e8a731f5022e209c8096
masondjoseph <mdjoseph@email.sc.edu>
2023-03-30 16:50:37 -0400

    feat: reverted startPlayingContext changes

--- lib/music/startPlayingContextTrack.ts
+++ lib/music/startPlayingContextTrack.ts
@@ -1,16 +1,13 @@
+"use client";
+
 import fetchClient from "@/lib/fetch/fetchClient";
 
-const startPlayingContextTrack = async (
-  contextUri: string,
-  offset: string,
-  trackUri: string
-) =>
+const startPlayingContextTrack = async (contextUri: string, trackUri: string) =>
   fetchClient(`https://api.spotify.com/v1/me/player/play`, {
     method: "PUT",
     body: JSON.stringify({
-      context_uri: contextUri || undefined,
-      offset: offset ? { uri: offset } : undefined,
-      uris: trackUri ? [trackUri] : undefined,
+      context_uri: contextUri,
+      offset: trackUri,
     }),
     cache: "no-cache",
   });
