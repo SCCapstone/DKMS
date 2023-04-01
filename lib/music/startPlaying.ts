dfc1ada91e260d50faa88304af86b0eada00bcd6
masondjoseph <mdjoseph@email.sc.edu>
2023-04-01 13:43:58 -0400

    feat: created startPlaying

---
+++ lib/music/startPlaying.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import fetchClient from "@/lib/fetch/fetchClient";
+
+type StartPlayingContextParams =
+  | {
+      uris: string[];
+      contextUri?: undefined;
+    }
+  | {
+      uris?: undefined;
+      contextUri: string;
+    }
+  | {
+      uris: string[];
+      contextUri: string;
+    };
+
+async function startPlaying(params: StartPlayingContextParams) {
+  return fetchClient(`https://api.spotify.com/v1/me/player/play`, {
+    method: "PUT",
+    body: JSON.stringify({
+      context_uri: params.contextUri,
+      uris: params.uris,
+    }),
+    cache: "no-cache",
+  });
+}
+export default startPlaying;
