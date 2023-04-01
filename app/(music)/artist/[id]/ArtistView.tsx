da9c7fe2ed73e0c56d6110bd6ac60738c59ea24c
masondjoseph <mdjoseph@email.sc.edu>
2023-04-01 16:13:48 -0400

    feat: added PlayButton to ArtistView

--- app/(music)/artist/[id]/ArtistView.tsx
+++ app/(music)/artist/[id]/ArtistView.tsx
@@ -2,7 +2,7 @@ import FollowButton from "@/components/FollowButton";
 import AudioFeatures from "@/components/music/AudioFeatures";
 import { AlbumList, TrackList } from "@/components/music/lists";
 import MusicHeader from "@/components/music/MusicHeader";
-import PlayContext from "@/components/music/PlayContext";
+import PlayButton from "@/components/music/PlayButton";
 import { formatNumber } from "@/lib/formatters";
 
 const ArtistView = ({
@@ -30,7 +30,7 @@ const ArtistView = ({
         isCircle: true,
         buttons: (
           <>
-            <PlayContext uri={artist.uri} />
+            <PlayButton uris={[artist.uri]} />
             <FollowButton
               id={artist.id}
               followType="artist"
