64d424af22b9de62c1bb76b9de592a964b648641
Dalton Craven <daltoncraven@proton.me>
2023-04-02 18:06:44 -0400

    feat: add recommendation buttons

--- lib/recommendations/getRecommendationsForUser.ts
+++ lib/recommendations/getRecommendationsForUser.ts
@@ -3,23 +3,13 @@ import { doc, getDoc } from "firebase/firestore";
 import { profilesCol } from "@/lib/firestore";
 
 import getRecommendationsBySeed from "./getRecommendationsBySeed";
+import { TARGET_MAPPING } from "./recommendationTargets";
+
+import type { TargetOption } from "./recommendationTargets";
 
 const DEFAULT_ARTIST = "4NHQUGzhtTLFvgF5SZesLK";
 const DEFAULT_TRACK = "0c6xIDDpzE81m2q797ordA";
 
-const TARGET_MAPPING = {
-  acoustic: { key: "target_acoustic", value: 0.8 },
-  danceable: { key: "target_danceability", value: 0.8 },
-  energetic: { key: "target_energy", value: 0.8 },
-  quiet: { key: "target_energy", value: 0.2 },
-  live: { key: "target_liveness", value: 0.8 },
-  popular: { key: "target_popularity", value: 80 },
-  niche: { key: "target_popularity", value: 20 },
-  happy: { key: "target_valence", value: 0.8 },
-  sad: { key: "target_valence", value: 0.2 },
-} as const;
-
-type TargetOption = keyof typeof TARGET_MAPPING;
 const isValidTarget = (target: string | undefined): target is TargetOption =>
   !!target && target in TARGET_MAPPING;
 
