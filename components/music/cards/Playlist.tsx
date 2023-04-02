5ce40e4efe04ba471f2d41dd4986d4fb256a69d6
masondjoseph <mdjoseph@email.sc.edu>
2023-04-02 12:28:21 -0400

    feat: fixed PlayButton import in Playlist

--- components/music/cards/Playlist.tsx
+++ components/music/cards/Playlist.tsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { getPlaiceholder } from "plaiceholder";
 
+import PlayButton from "@/components/music/PlayButton";
 import ShareIcon from "@/components/ui/shareIcon";
 import Skeleton from "@/components/ui/Skeleton";
-
-import PlayButton from "../PlayButton";
 
 const Playlist = async ({
   playlist,
