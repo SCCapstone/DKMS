78f3fa264816b8adff4b6da85e6d8b6ffb6772ab
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-04-02 16:58:30 -0400

    fix: deleted random space

--- components/music/grids/PlaylistsGrid.tsx
+++ components/music/grids/PlaylistsGrid.tsx
@@ -18,7 +18,6 @@ const PlaylistsGrid = ({
         isHalf ? "" : "md:grid-cols-3 lg:grid-cols-4"
       } gap-4`}
     >
-      {" "}
       {playlists.map((playlist, index) => (
         // @ts-expect-error Next 13 handles async components
         <Playlist key={playlist?.id ?? index} playlist={playlist} />
