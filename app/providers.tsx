c58d89a7c2964cd639f27cf95e7fec34a7d9385f
Dalton Craven <daltoncraven@proton.me>
2023-04-13 10:49:48 -0400

    refactor: dynamically import react-hot-toast Toaster

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { Toaster } from "react-hot-toast";
 
 import SidebarProvider from "@/components/Sidebar/SidebarProvider";
 import { THEME_VALUES } from "@/lib/theme";
+
+const Toaster = dynamic(
+  async () => {
+    const { Toaster: BaseToaster } = await import("react-hot-toast");
+    return BaseToaster;
+  },
+  {
+    ssr: false,
+  }
+);
 
 const Providers = ({ children }: { children: React.ReactNode }) => (
   <SessionProvider>
