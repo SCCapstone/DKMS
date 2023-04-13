827316b41d4cb5ec8a257d8f944316524a01501a
Dalton Craven <daltoncraven@proton.me>
2023-04-13 16:35:29 -0400

    fix: fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,8 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   callbacks: {
     authorized: ({ token }) =>
-      (!!token && token.error !== "RefreshAccessTokenError") || true,
+      !!token && token.error !== "RefreshAccessTokenError",
   },
 });
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/app/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
-};
+export const config = { matcher: ["/app:path*"] };
