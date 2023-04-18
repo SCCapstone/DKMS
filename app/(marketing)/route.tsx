b04b29b6191118abd16dd01e29888fc7d577969e
Dalton Craven <daltoncraven@proton.me>
2023-04-18 00:45:47 -0400

    fix: fallback to vercel default URL

--- app/(marketing)/route.tsx
+++ app/(marketing)/route.tsx
@@ -2,9 +2,12 @@ import { NextResponse } from "next/server";
 
 // eslint-disable-next-line import/prefer-default-export
 export function GET() {
-  if (!process.env.NEXTAUTH_URL) {
-    throw new Error("NEXTAUTH_URL is not defined");
+  if (!process.env.NEXTAUTH_URL && !process.env.NEXT_PUBLIC_VERCEL_URL) {
+    throw new Error("No base url defined");
   }
 
-  return NextResponse.redirect(new URL("/app", process.env.NEXTAUTH_URL));
+  const baseUrl =
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.NEXTAUTH_URL;
+
+  return NextResponse.redirect(new URL("/app", baseUrl));
 }
--- app/app/profile/route.tsx
+++ app/app/profile/route.tsx
@@ -5,11 +5,15 @@ import { getCurrentUser } from "@/lib/getUser";
 // eslint-disable-next-line import/prefer-default-export
 export async function GET() {
   const user = await getCurrentUser();
-  if (!process.env.NEXTAUTH_URL) {
-    throw new Error("NEXTAUTH_URL is not defined");
+
+  if (!process.env.NEXTAUTH_URL && !process.env.NEXT_PUBLIC_VERCEL_URL) {
+    throw new Error("No base url defined");
   }
 
+  const baseUrl =
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.NEXTAUTH_URL;
+
   return NextResponse.redirect(
-    new URL(`/app/profile/${user.username}`, process.env.NEXTAUTH_URL)
+    new URL(`/app/profile/${user.username}`, baseUrl)
   );
 }
