af20cb81fb2f88134937234c71ea6c1ad24cbd45
Dalton Craven <daltoncraven@proton.me>
2023-04-18 00:27:30 -0400

    fix: fix routing in production environment

--- app/(marketing)/route.tsx
+++ app/(marketing)/route.tsx
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 
 // eslint-disable-next-line import/prefer-default-export
-export function GET(request: Request) {
-  return NextResponse.redirect(new URL("/app", request.url));
+export function GET() {
+  if (!process.env.NEXTAUTH_URL) {
+    throw new Error("NEXTAUTH_URL is not defined");
+  }
+
+  return NextResponse.redirect(new URL("/app", process.env.NEXTAUTH_URL));
 }
--- app/app/profile/route.tsx
+++ app/app/profile/route.tsx
@@ -3,9 +3,13 @@ import { NextResponse } from "next/server";
 import { getCurrentUser } from "@/lib/getUser";
 
 // eslint-disable-next-line import/prefer-default-export
-export async function GET(request: Request) {
+export async function GET() {
   const user = await getCurrentUser();
+  if (!process.env.NEXTAUTH_URL) {
+    throw new Error("NEXTAUTH_URL is not defined");
+  }
+
   return NextResponse.redirect(
-    new URL(`/app/profile/${user.username}`, request.url)
+    new URL(`/app/profile/${user.username}`, process.env.NEXTAUTH_URL)
   );
 }
