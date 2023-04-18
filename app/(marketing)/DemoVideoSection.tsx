7ac0542755e2f995a0e6d8b528d5e0ee776cd8d0
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-04-18 17:46:37 -0400

    feat: created section for final demo video

--- app/(marketing)/DemoVideoSection.tsx
+++ app/(marketing)/DemoVideoSection.tsx
@@ -1,18 +1,27 @@
+import Image from "next/image";
+
 const DemoVideoSection = () => (
-  <div className="hero min-h-screen bg-base-300">
-    <div className="hero-content text-center">
-      <div className="max-w-lg">
-        <div className="card bg-base-100 shadow-xl w-6/12 object-cover">
-          <div className="card-body">
-            <h2 className="card-title">Final Demo Video</h2>
-            <p>
-              Watch our final demo video, where one of our developers walks
-              through our application, explaining and using each of the
-              features.
-            </p>
-          </div>
+  <div className="bg-base-100 pb-10">
+    <h2 className="text-5xl font-bold text-right py-10 pr-10 text-primary">
+      Final Demo Video
+    </h2>
+    <div className="grid grid-cols-2">
+      <div className="pl-20">
+        <Image
+          width="750"
+          height="250"
+          src="/images/defaults/videoPlaceholder.png"
+          alt="video placeholder"
+        />
+      </div>
+      <div className="card bg-base-300 shadow-xl">
+        <div className="card-body">
+          <p>
+            Watch our final demo video, where one of our developers walks
+            through our application by explaining and using each of the
+            features.
+          </p>
         </div>
-        <div />
       </div>
     </div>
   </div>
