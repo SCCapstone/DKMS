fd7b38fab560c18e171110ba6a85da673e8f06ce
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-04-18 16:58:37 -0400

    feat: reconfigured section for pretty screenshots

--- app/(marketing)/ScreenshotSection.tsx
+++ app/(marketing)/ScreenshotSection.tsx
@@ -1,58 +1,54 @@
 import Image from "next/image";
 
 const ScreenshotSection = () => (
-  <div>
-    <div className="carousel w-full p-4 space-x-4 bg-neutral rounded-box hero min-h-screen">
-      <div id="item1" className="carousel-item ">
-        <Image
-          src="/images/defaults/exampleSearch.png"
-          className="rounded-box"
-          alt="search-screenshot"
-          width={1920}
-          height={966}
-        />
+  <div className="bg-base-300 rounded-b-box rounded-tr-box relativeo">
+    <div className="preview border-base-300 bg-base-200 rounded-b-box rounded-tr-box flex flex-wrap items-center justify-center gap-2 border bg-cover bg-top p-4 undefined">
+      <div className="flex flex-col items-center">
+        <h1 className="text-5xl font-bold text-secondary pb-4">Preview</h1>
+        <p className="pb-2">
+          Scroll side to side to see examples from our application!
+        </p>
       </div>
-      <div id="item2" className="carousel-item ">
-        <Image
-          src="/images/defaults/exampleRecs.png"
-          className="rounded-box"
-          alt="search-screenshot"
-          width={1920}
-          height={966}
-        />
+
+      <div className="carousel carousel-center p-4 space-x-4 bg-neutral rounded-box">
+        <div className="carousel-item">
+          <Image
+            src="/images/defaults/exampleProfile.png"
+            className="rounded-box"
+            width={960}
+            height={480}
+            alt="example profile"
+          />
+        </div>
+        <div className="carousel-item">
+          <Image
+            src="/images/defaults/exampleSearch.png"
+            className="rounded-box"
+            width={960}
+            height={480}
+            alt="example search"
+          />
+        </div>
+        <div className="carousel-item">
+          <Image
+            src="/images/defaults/exampleArtist.png"
+            className="rounded-box"
+            width={960}
+            height={480}
+            alt="example artist"
+          />
+        </div>
+
+        <div className="carousel-item">
+          <Image
+            src="/images/defaults/exampleRecs.png"
+            className="rounded-box"
+            width={960}
+            height={480}
+            alt="example recommendations"
+          />
+        </div>
       </div>
-      <div id="item3" className="carousel-item ">
-        <Image
-          src="/images/defaults/exampleProfile.png"
-          className="rounded-box"
-          alt="search-screenshot"
-          width={1920}
-          height={966}
-        />
-      </div>
-      <div id="item4" className="carousel-item ">
-        <Image
-          src="/images/defaults/exampleArtist.png"
-          className="rounded-box"
-          alt="search-screenshot"
-          width={1920}
-          height={966}
-        />
-      </div>
-    </div>
-    <div className="flex justify-center w-full py-2 gap-2">
-      <a href="#item1" className="btn btn-xs">
-        1
-      </a>
-      <a href="#item2" className="btn btn-xs">
-        2
-      </a>
-      <a href="#item3" className="btn btn-xs">
-        3
-      </a>
-      <a href="#item4" className="btn btn-xs">
-        4
-      </a>
     </div>
   </div>
 );
