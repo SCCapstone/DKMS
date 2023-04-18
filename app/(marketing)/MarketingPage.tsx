404b2dbea89e23df34f1ce2728e76f3b0d7b4dc3
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-04-18 16:59:37 -0400

    feat: created a why use section

--- app/(marketing)/MarketingPage.tsx
+++ app/(marketing)/MarketingPage.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-
-import SignInButton from "./auth/signin/SignInButton";
 import DemoVideoSection from "./DemoVideoSection";
+import HowSection from "./HowSection";
 import ScreenshotSection from "./ScreenshotSection";
 import TitleSection from "./TitleSection";
+import WhySection from "./WhySection";
 
 const MarketingPage = () => (
   <div className="flex flex-col">
     <TitleSection />
-    <DemoVideoSection />
+    <WhySection />
     <ScreenshotSection />
+    <DemoVideoSection />
   </div>
 );
 
