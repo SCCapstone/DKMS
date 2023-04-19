20594e72864a19588cc76a1252571c9395fa54c7
Dalton Craven <daltoncraven@proton.me>
2023-04-18 21:19:57 -0400

    fix: don't wrap ArtistLinks in p

--- app/app/(music)/playlist/[id]/PlaylistView.tsx
+++ app/app/(music)/playlist/[id]/PlaylistView.tsx
@@ -28,10 +28,10 @@ const PlaylistView = ({
         content: playlist.description ? (
           playlist.description
         ) : (
-          <p>
+          <>
             Featuring{" "}
             <ArtistLinks artists={tracks.flatMap((track) => track.artists)} />
-          </p>
+          </>
         ),
         musicItemId: playlist.id,
         musicItemType: "playlist",
