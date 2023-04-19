fda2ca416f7096edb26bab4466af4beba6c50638
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-04-18 20:23:00 -0400

    fix: small design tweaks

--- app/(marketing)/HowSection.tsx
+++ app/(marketing)/HowSection.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const HowSection = () => (
   <div className="bg-base-300 pb-10">
     <h1 className="text-5xl font-bold text-left py-10 pl-10">How to use</h1>
-    <div className="grid grid-cols-2 gap-20">
-      <div className="card bg-base-100 shadow-xl">
+    <div className="grid grid-cols-1 gap-y-10 px-20">
+      <div className="card card-side bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Create a post</h2>
           <p>
@@ -13,24 +13,24 @@ const HowSection = () => (
             feed!
           </p>
         </div>
+        <figure>
+          <Image
+            src="/images/defaults/exampleProfile.png"
+            width={480}
+            height={240}
+            alt="example feed"
+          />
+        </figure>
       </div>
-      <div>
-        <Image
-          src="/images/defaults/exampleProfile.png"
-          width={480}
-          height={240}
-          alt="example feed"
-        />
-      </div>
-      <div>
-        <Image
-          src="/images/defaults/exampleSearch.png"
-          width={480}
-          height={240}
-          alt="example search"
-        />
-      </div>
-      <div className="card bg-base-100 shadow-xl">
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <Image
+            src="/images/defaults/exampleSearch.png"
+            width={480}
+            height={240}
+            alt="example search"
+          />
+        </figure>
         <div className="card-body">
           <h2 className="card-title">Search for music</h2>
           <p>
@@ -39,7 +39,7 @@ const HowSection = () => (
           </p>
         </div>
       </div>
-      <div className="card bg-base-100 shadow-xl">
+      <div className="card card-side bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Use playback</h2>
           <p>
@@ -48,14 +48,14 @@ const HowSection = () => (
             &ldquo;DKMS&rdquo;. Now play music to your heart&apos;s content!
           </p>
         </div>
-      </div>
-      <div>
-        <Image
-          src="/images/defaults/exampleProfile.png"
-          width={480}
-          height={240}
-          alt="example feed"
-        />
+        <figure>
+          <Image
+            src="/images/defaults/exampleFeed.png"
+            width={480}
+            height={240}
+            alt="example feed"
+          />
+        </figure>
       </div>
     </div>
   </div>
--- app/(marketing)/TitleSection.tsx
+++ app/(marketing)/TitleSection.tsx
@@ -2,18 +2,20 @@ import Link from "next/link";
 
 const TitleSection = () => (
   <div className="hero min-h-screen bg-base-100">
-    <div className="hero-content text-center">
-      <div className="max-w-md">
-        <h1 className="text-5xl font-bold text-primary">
-          Do more with your Spotify
-        </h1>
-        <p className="py-6">
-          Connect with others and discover more about your music circle than
-          ever.
-        </p>
-        <Link className="btn btn-secondary normal-case" href="/auth/signin">
-          SIGN IN
-        </Link>
+    <div className="hero-content">
+      <div className="card max-w-md bg-base-300">
+        <div className="card-body text-center">
+          <h1 className="card-title text-5xl font-bold text-primary">
+            Do more with your Spotify
+          </h1>
+          <p className="py-6">
+            Connect with others and discover more about your music circle than
+            ever.
+          </p>
+          <Link className="btn btn-secondary normal-case" href="/auth/signin">
+            SIGN IN
+          </Link>
+        </div>
       </div>
     </div>
   </div>
