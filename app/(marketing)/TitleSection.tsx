37f80820a0dbc6df07ecc54384a7af9962a85e8a
Dalton Craven <daltoncraven@proton.me>
2023-04-20 18:53:49 -0400

    feat: make hero glass

--- app/(marketing)/TitleSection.tsx
+++ app/(marketing)/TitleSection.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 
 const TitleSection = () => (
-  <div className="hero h-full bg-gradient-to-r from-primary to-secondary">
+  <div className="hero h-full bg-gradient-to-bl via-primary from-25% from-secondary shadow-inner">
     <div className="hero-content">
-      <div className="card max-w-md bg-base-300">
-        <div className="card-body text-center">
-          <h1 className="card-title text-5xl font-bold text-secondary">
+      <div className="card glass max-w-md shadow-xl hover:shadow-xl">
+        <div className="card-body text-center text-primary-content">
+          <h1 className="card-title text-5xl font-bold">
             Dance more with DKMS.
           </h1>
           <p className="py-6">
             Connect with others and discover more about your music circle than
             ever.
           </p>
-          <Link className="btn btn-secondary normal-case" href="/auth/signin">
-            SIGN IN
+          <Link className="btn btn-secondary" href="/auth/signin">
+            Sign In
           </Link>
         </div>
       </div>
