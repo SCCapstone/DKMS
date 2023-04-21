a723a204cc0907315316c119589160534e53bad7
Dalton Craven <daltoncraven@proton.me>
2023-04-21 17:41:37 -0400

    fix: prevent resizing when sidebar is open

--- components/music/lists/TrackList.tsx
+++ components/music/lists/TrackList.tsx
@@ -18,56 +18,67 @@ const TrackList = ({
   showNumber?: boolean;
   showAlbum?: boolean;
 }) => (
-  <div className="overflow-x-auto md:w-[calc(100vw-20rem)]">
-    <table className="table table-compact w-full [&_th]:first:relative">
-      <thead>
-        <tr>
-          {showNumber && <th>#</th>}
-          <th>Title</th>
-          <th>Artist</th>
-          {showAlbum && "album" in tracks[0] && <th>Album</th>}
-          {"added_at" in tracks[0] && <th>Date added</th>}
-          {"added_by" in tracks[0] && <th>Added by</th>}
-          <th className="text-right">Duration</th>
-          <th className="text-center">Play</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tracks.map((track, index) => (
-          <tr className="hover" key={track.id}>
-            {showNumber && <td>{index + 1}</td>}
-            <td>
-              <a
-                className="font-bold text-primary"
-                href={`/app/track/${track.id}`}
-              >
-                {track.name}
-              </a>
-            </td>
-            <td>
-              <ArtistLinks artists={track.artists} />
-            </td>
-            {showAlbum && "album" in track && (
+  <div className="max-w-[40vw] rounded-xl mx-auto bg-neutral p-4">
+    <div className="overflow-x-auto rounded-xl">
+      <table className="table table-compact w-full [&_th]:first:relative">
+        <thead>
+          <tr>
+            {showNumber && <th>#</th>}
+            <th>Title</th>
+            <th>Artist</th>
+            {showAlbum && "album" in tracks[0] && <th>Album</th>}
+            {"added_at" in tracks[0] && <th>Date added</th>}
+            {"added_by" in tracks[0] && <th>Added by</th>}
+            <th className="text-right">Duration</th>
+            <th className="text-center">Play</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tracks.map((track, index) => (
+            <tr className="hover" key={track.id}>
+              {showNumber && <td>{index + 1}</td>}
               <td>
-                <a className="font-bold" href={`/app/album/${track.album.id}`}>
-                  {track.album.name}
+                <a
+                  className="font-bold text-primary"
+                  href={`/app/track/${track.id}`}
+                >
+                  {track.name}
                 </a>
               </td>
-            )}
-            {"added_at" in track && <td>{formatDate(track.added_at)}</td>}
-            {"added_by" in track && <td>{track.added_by.id}</td>}
-            <td className="text-right">{formatDuration(track.duration_ms)}</td>
-            <td className="text-center">
-              {contextUri ? (
-                <PlayButton contextUri={contextUri} offset={track.uri} small />
-              ) : (
-                <PlayButton uris={[track.uri]} small />
+              <td>
+                <ArtistLinks artists={track.artists} />
+              </td>
+              {showAlbum && "album" in track && (
+                <td>
+                  <a
+                    className="font-bold"
+                    href={`/app/album/${track.album.id}`}
+                  >
+                    {track.album.name}
+                  </a>
+                </td>
               )}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              {"added_at" in track && <td>{formatDate(track.added_at)}</td>}
+              {"added_by" in track && <td>{track.added_by.id}</td>}
+              <td className="text-right">
+                {formatDuration(track.duration_ms)}
+              </td>
+              <td className="text-center">
+                {contextUri ? (
+                  <PlayButton
+                    contextUri={contextUri}
+                    offset={track.uri}
+                    small
+                  />
+                ) : (
+                  <PlayButton uris={[track.uri]} small />
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </div>
 );
 
