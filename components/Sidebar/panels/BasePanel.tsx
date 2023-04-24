bc2b23754cfa362d2b5a80545875981bde9e96a6
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-04-23 22:59:57 -0400

    refactor: refactor error boundaries

--- components/Sidebar/panels/BasePanel.tsx
+++ components/Sidebar/panels/BasePanel.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ErrorBoundary } from "react-error-boundary";
+
 import { useSidebar } from "../SidebarContext";
 
 import type { SidebarOptions } from "../types";
@@ -17,15 +19,17 @@ const BasePanel = ({
 
   if (sidebar !== sidebarId) return null;
   return (
-    <div className="w-full md:w-64 max-h-screen h-screen overflow-y-scroll md:p-4 md:bg-neutral md:text-neutral-content">
-      {title && (
-        <>
-          <h2 className="normal-case font-bold">{title}</h2>
-          <div className="divider" />
-        </>
-      )}
-      {children}
-    </div>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <div className="w-full md:w-64 max-h-screen h-screen overflow-y-scroll md:p-4 md:bg-neutral md:text-neutral-content">
+        {title && (
+          <>
+            <h2 className="normal-case font-bold">{title}</h2>
+            <div className="divider" />
+          </>
+        )}
+        {children}
+      </div>
+    </ErrorBoundary>
   );
 };
 
--- components/Sidebar/panels/Friends/index.tsx
+++ components/Sidebar/panels/Friends/index.tsx
@@ -1,5 +1,3 @@
-import { ErrorBoundary } from "react-error-boundary";
-
 import getFollowedUsers from "@/lib/followers/getFollowedUsers";
 
 import BasePanel from "../BasePanel";
@@ -9,16 +7,14 @@ import Friend from "./Friend";
 const Friends = async () => {
   const users = await getFollowedUsers();
   return (
-    <ErrorBoundary fallback={<div>Something went wrong</div>}>
-      <BasePanel title="Friends" sidebarId="friends">
-        <ul>
-          {users.map((user) => (
-            // @ts-expect-error Next 13 Server Component
-            <Friend key={user.id} username={user.username} userId={user.id} />
-          ))}
-        </ul>
-      </BasePanel>
-    </ErrorBoundary>
+    <BasePanel title="Friends" sidebarId="friends">
+      <ul>
+        {users.map((user) => (
+          // @ts-expect-error Next 13 Server Component
+          <Friend key={user.id} username={user.username} userId={user.id} />
+        ))}
+      </ul>
+    </BasePanel>
   );
 };
 
