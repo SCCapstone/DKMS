30e460b0d7307a1ed001f6e169e7d82a82894174
Dalton Craven <daltoncraven@proton.me>
2023-04-23 20:57:42 -0400

    DROPME: make queue throw an error

--- lib/playback/getQueue.ts
+++ lib/playback/getQueue.ts
@@ -4,7 +4,12 @@ const getQueue = async () => {
   const data = await fetchServer<SpotifyApi.UsersQueueResponse>(
     "https://api.spotify.com/v1/me/player/queue"
   );
-  return data;
+  return {
+    queue: [null, ...data.queue] as (
+      | SpotifyApi.TrackObjectFull
+      | SpotifyApi.EpisodeObjectFull
+    )[],
+  };
 };
 
 export default getQueue;
