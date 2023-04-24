454ff59ff44b73f14db77df017ce2beb03dab129
claycrews2002 <claycrews2002@gmail.com>
2023-04-24 01:41:38 -0400

    fix: test with getSession

--- app/app/profile/route.tsx
+++ app/app/profile/route.tsx
@@ -1,19 +1,19 @@
 import { NextResponse } from "next/server";
-import { useSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 import type { NextRequest } from "next/server";
 
 // eslint-disable-next-line import/prefer-default-export
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   /**  For local production builds using `next start`,
    * the `request.nextUrl.origin` will be `0.0.0.0`, which
    * is not a valid domain. We need to replace it with `localhost`.
    */
   const baseUrl = request.nextUrl.origin.replace("0.0.0.0", "localhost");
 
-  const { data: session, status } = useSession();
+  const session = await getSession();
 
-  if (status === "authenticated") {
+  if (session) {
     return NextResponse.redirect(
       new URL(`/app/profile/${session.user.username}`, baseUrl)
     );
