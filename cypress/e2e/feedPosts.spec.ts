87f8d33993da8dbf81085a62dffe330f1765d3d9
claycrews2002 <claycrews2002@gmail.com>
2023-04-24 00:11:31 -0400

    refactor: merge to one it

--- cypress/e2e/feedPosts.spec.ts
+++ cypress/e2e/feedPosts.spec.ts
@@ -1,22 +1,20 @@
-const postText = `Testing: ${Math.random().toString(36).substring(2, 8)}`;
-const commentText = `Testing: ${Math.random().toString(36).substring(2, 8)}`;
 describe("Post and Delete Feed Item and Comment", () => {
   beforeEach(() => {
     // Log in before each test
     cy.auth();
   });
 
-  it("should start on the feed page", () => {
+  it("should post and delete feed item and comment", () => {
+    const postText = `Testing: ${Math.random().toString(36).substring(2, 8)}`;
+    const commentText = `Testing: ${Math.random()
+      .toString(36)
+      .substring(2, 8)}`;
+
     // Start from the feed page
     cy.visit("/app");
     // The feed page should contain an h1 with "Friends"
     cy.get("h1").contains("Friends");
-  });
 
-  it("should post a feed item", () => {
-    // Start from the feed page
-
-    cy.visit("/app");
     // Post a feed item
     cy.get("textarea[id=comment]").click();
     cy.get("textarea[id=comment]").type(postText);
@@ -25,12 +23,7 @@ describe("Post and Delete Feed Item and Comment", () => {
 
     // A feed item should be posted with random string for testing
     cy.get("p").contains(postText);
-  });
 
-  it("should post a comment", () => {
-    // Start from the feed page
-
-    cy.visit("/app");
     // Post a comment
     cy.get("textarea[id=chat]").eq(0).click();
     cy.get("textarea[id=chat]").eq(0).type(commentText);
@@ -39,22 +32,12 @@ describe("Post and Delete Feed Item and Comment", () => {
 
     // A feed comment should be posted with a random string for testing
     cy.get("p").contains(commentText);
-  });
-
-  it("should delete a comment", () => {
-    // Start from the feed page
-    cy.visit("/app");
 
     // Delete the comment that was just posted
     cy.get(`button[title="Delete feed item"]`).eq(1).click();
 
     // A comment with the random strings for a comment should be deleted
     cy.contains(commentText).should("not.exist");
-  });
-
-  it("should delete a feed item", () => {
-    // Start from the feed page
-    cy.visit("/app");
 
     // Delete the feed item that was just posted
     cy.get(`button[title="Delete feed item"]`).eq(0).click();
