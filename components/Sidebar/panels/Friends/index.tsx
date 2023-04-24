b7fad1a57ab125aab3cd63ff7f8f669c4c831a59
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-04-23 22:41:33 -0400

    feat: added error boundaries for side panel

--- components/Sidebar/panels/Friends/index.tsx
+++ components/Sidebar/panels/Friends/index.tsx
@@ -1,3 +1,5 @@
+import { ErrorBoundary } from "react-error-boundary";
+
 import getFollowedUsers from "@/lib/followers/getFollowedUsers";
 
 import BasePanel from "../BasePanel";
@@ -7,14 +9,16 @@ import Friend from "./Friend";
 const Friends = async () => {
   const users = await getFollowedUsers();
   return (
-    <BasePanel title="Friends" sidebarId="friends">
-      <ul>
-        {users.map((user) => (
-          // @ts-expect-error Next 13 Server Component
-          <Friend key={user.id} username={user.username} userId={user.id} />
-        ))}
-      </ul>
-    </BasePanel>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <BasePanel title="Friends" sidebarId="friends">
+        <ul>
+          {users.map((user) => (
+            // @ts-expect-error Next 13 Server Component
+            <Friend key={user.id} username={user.username} userId={user.id} />
+          ))}
+        </ul>
+      </BasePanel>
+    </ErrorBoundary>
   );
 };
 
