38901aeaa3bccca39a4fb2cd7a90cd1dc057a949
Dalton Craven <daltoncraven@proton.me>
2023-04-24 22:52:19 -0400

    fix: fix share spec

--- cypress/e2e/share.spec.ts
+++ cypress/e2e/share.spec.ts
@@ -4,11 +4,45 @@ describe("Share Music to Feed", () => {
     cy.auth();
   });
 
-  it("should start on the search page", () => {
+  it("should search and share a track to feed", () => {
     // Start from the search page
     cy.visit("/app/search");
-    // The search page should contain an h1 with "Search"
-    cy.get("h1").contains("Search");
+    // Search for an artist
+    cy.get("input").type("Nirvana");
+
+    cy.get("button").contains("Search").click();
+
+    // The search results should contain a link with "Nirvana"
+    cy.get("a").contains("Nirvana");
+    cy.url().should("include", "/search?q=Nirvana");
+
+    // Share the track to feed
+    cy.get(`button[title="Share to feed"]`).eq(0).click();
+    // Add text to the post
+    const postText = `Testing: ${Math.random().toString(36).substring(2, 8)}`;
+    cy.get("textarea[id=comment]").click();
+    cy.get("textarea[id=comment]").type(postText);
+
+    cy.get("button").contains("Share").click();
+
+    // Visit feed page to view post
+    cy.visit("/app");
+    cy.reload();
+
+    // A feed item with the text postText should be present
+    cy.get("p").contains(postText);
+
+    // A feed item with a card with the track information should be present
+    cy.get('div[class*="card"]').find("img").eq(0).click();
+    // The url should be on the track's page
+    cy.url().should("include", "/track");
+
+    // Visit the feed page
+    cy.visit("/app");
+    // Delete the feed item that was just posted
+    cy.get(`button[title="Delete feed item"]`).eq(0).click();
+    // A feed item with postText should be deleted
+    cy.get("p", { timeout: 10000 }).contains(postText).should("not.exist");
   });
 
   it("should search and share an album to feed", () => {
@@ -84,47 +118,6 @@ describe("Share Music to Feed", () => {
     cy.get('div[class*="card"]').find("img").eq(0).click();
     // The url should be on the artist's page
     cy.url().should("include", "/artist");
-
-    // Visit the feed page
-    cy.visit("/app");
-    // Delete the feed item that was just posted
-    cy.get(`button[title="Delete feed item"]`).eq(0).click();
-    // A feed item with postText should be deleted
-    cy.get("p", { timeout: 10000 }).contains(postText).should("not.exist");
-  });
-
-  it("should search and share a track to feed", () => {
-    // Start from the search page
-    cy.visit("/app/search");
-    // Search for an artist
-    cy.get("input").type("Nirvana");
-
-    cy.get("button").contains("Search").click();
-
-    // The search results should contain a link with "Nirvana"
-    cy.get("a").contains("Nirvana");
-    cy.url().should("include", "/search?q=Nirvana");
-
-    // Share the track to feed
-    cy.get(`button[title="Share to feed"]`).eq(0).click();
-    // Add text to the post
-    const postText = `Testing: ${Math.random().toString(36).substring(2, 8)}`;
-    cy.get("textarea[id=comment]").click();
-    cy.get("textarea[id=comment]").type(postText);
-
-    cy.get("button").contains("Share").click();
-
-    // Visit feed page to view post
-    cy.visit("/app");
-    cy.reload();
-
-    // A feed item with the text postText should be present
-    cy.get("p").contains(postText);
-
-    // A feed item with a card with the track information should be present
-    cy.get('div[class*="card"]').find("img").eq(0).click();
-    // The url should be on the track's page
-    cy.url().should("include", "/track");
 
     // Visit the feed page
     cy.visit("/app");
