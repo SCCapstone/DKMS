ed5d23325d217cf83b4449beaeda5726882f7967
Dalton Craven <daltoncraven@proton.me>
2023-04-24 23:22:23 -0400

    feat: add auth layout

---
+++ app/(marketing)/(navlayout)/auth/layout.tsx
@@ -0,0 +1,7 @@
+/* Marketing Page Layout for auth pages */
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => (
+  <div className="container mx-auto pt-5">{children}</div>
+);
+
+export default AuthLayout;
